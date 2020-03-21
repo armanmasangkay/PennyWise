13f804b274ed53abc8b1041faffe670bfd047d2c
armanmasangkay <vss.arman@gmail.com>
2020-03-21 16:13:01 +0800

    Adding some verification functionality to the login page

--- phpfiles/database_connect.php
+++ phpfiles/database_connect.php
@@ -3,16 +3,15 @@
 $SERVER_NAME="localhost:3306";
 $DB_USERNAME="root";
 $DB_PASSWORD="namra370h55v";
-$conn;
 
 
 function connect(){
-    global $SERVER_NAME, $USERNAME,$PASSWORD;
+    global $SERVER_NAME, $DB_USERNAME,$DB_PASSWORD;
     try {
         $conn = new PDO("mysql:host=$SERVER_NAME;dbname=savinggoal", $DB_USERNAME, $DB_PASSWORD);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return true;
+        return $conn;
     }
     catch(PDOException $e){
        return false;
--- phpfiles/login_check.php
+++ phpfiles/login_check.php
@@ -2,8 +2,37 @@
 
 require 'database_connect.php';
 
+
+$conn=connect();
 $username=$_POST['username'];
 $password=$_POST['password'];
+
+
+//checks login information
+function checkLoginInfo($username,$password){
+    global $conn;
+    $stmt = $conn->prepare("SELECT username,password FROM User");
+    $stmt->execute();
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach($stmt->fetchAll() as $k=>$v) {
+        if ($v['username']==$username && $v['password']==$password){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
+
+
+
+if (checkLoginInfo($username,$password)==true){
+    echo json_encode(array('success'=>1));
+}else{
+    echo json_encode(array('success'=>0));
+}
+
 
 
 
